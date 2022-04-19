47eba460d27e913b70984fd9114190c2a07f9c7d
m-chenh <103270083+m-chenh@users.noreply.github.com>
2022-04-19 16:08:56 -0700

    Fixed Error #3 (infinite While Loop)
    When using brackets in text w/o embedding a link, fixed infinite while loop:
    [https://something.com, some-thing.html, www.google.com]

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,9 +16,26 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(markdown.charAt(openBracket-1) != '!')
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if (openParen == -1 || closeParen == -1) 
+            {
+                break;
+            }
+            else if(closeBracket+1 < markdown.length())
+                if (markdown.charAt(closeBracket+1) != '(')
+                {
+                    currentIndex = markdown.indexOf("[", closeBracket);
+                }
+            else
+            {
+                currentIndex = closeParen + 1;
+                if(openBracket-1 >= 0)
+                {
+                    if(markdown.charAt(openBracket-1) != '!')
+                        toReturn.add(markdown.substring(openParen + 1, closeParen));
+                }
+                else if (openBracket == 0)
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
         }
 
         return toReturn;
